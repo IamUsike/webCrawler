451c835c84765e0bec6dab647b66ec260674cecd
musick <sathwikthanmay4@gmail.com>
2024-12-09 10:58:59 +0530

    getURLFromHTML

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawler.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawler.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol', ()=>{
@@ -28,4 +28,78 @@ test('normalizeURL strip capitals', ()=>{ //url constructor does this by default
     const expected = "blog.boot.dev/path"
     expect(actual).toEqual(expected)
 })
+
+test('getURLsfromHTML, absolute', ()=>{ 
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html?>
+    `
+
+    const inputBaseURL = `https://blog.boot.dev/path`
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = [`https://blog.boot.dev/path/`]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML, relative', ()=>{ 
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html?>
+    `
+
+    const inputBaseURL = `https://blog.boot.dev`
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = [`https://blog.boot.dev/path/`]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML, both (rel&abs)', ()=>{ 
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog path1
+            <a href="/path2/">
+                Boot.dev Blog path2
+            </a>
+        </body>
+    </html?>
+    `;
+
+    const inputBaseURL = `https://blog.boot.dev`
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = [
+      `https://blog.boot.dev/path1/`,
+      `https://blog.boot.dev/path2/`,
+    ];
+    expect(actual).toEqual(expected)
+})
+
+
+test("getURLsfromHTML, invalid", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="invalid">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html?>
+    `;
+
+  const inputBaseURL = `https://blog.boot.dev`;
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
+  expect(actual).toEqual(expected);
+});
 
--- crawler.js
+++ crawler.js
@@ -1,16 +1,46 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom; //allows node to access dom apis
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative URL
+      try{
+      const urlObj = new URL(`${baseURL}${linkElement.href}`);
+      urls.push(`${baseURL}${linkElement.href}`);
+      }catch(err){
+        console.log(`Error with relative URLs: ${err.message}`)
+      }
+      
+    } else {
+      //absolute URLs 
+      try {
+        const urlObj = new URL(linkElement.href)
+        urls.push(linkElement.href)
+      } catch (error) {
+        console.log(`Fucked up absolute URL: ${error.message}`)
+      }
+    }
+  }
+  return urls;
+}
+
 /* 1. There may be different url pointing to the same page 
     we need to normalize those 
     2. Strip the trailing slash*/
 
-
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString);
-    const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
-    if(hostPath.length > 0 && hostPath.slice(-1)==='/')
-        return hostPath.slice(0,hostPath.length-1)
-    return hostPath;
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/")
+    return hostPath.slice(0, hostPath.length - 1);
+  return hostPath;
 }
 
 module.exports = {
-    normalizeURL
-}
+  normalizeURL,
+  getURLsFromHTML,
+};
